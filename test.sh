796929afa887b9405610fa2ba25f38a6a7c5cd53
Josh Baker <joshbaker77@gmail.com>
2016-03-04 16:38:23 -0700

    more X's

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ if [ ! -f "tile38-server" ];then
 	./build.sh
 fi
 
-TMP="$(mktemp -d -t data-test)"
+TMP="$(mktemp -d -t data-test.XXXX)"
 ./tile38-server -p 9876 -d "$TMP" -q &
 PID=$!
 function end {

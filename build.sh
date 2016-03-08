162528bdeb3adc3ccda95715c4321c99ce3a6ca6
Josh Baker <joshbaker77@gmail.com>
2016-03-08 06:12:39 -0700

    fixed ldflags

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 set -e
 
 VERSION="0.0.1"
-PROTECTED_MODE="no" 
+PROTECTED_MODE="no"
+
+# Hardcode some values to the core package
+LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.Version=${VERSION}"
+LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.GitSHA=$(git rev-parse --short HEAD)"
+LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.BuildTime=$(date +%FT%T%z)"
+if [ "$PROTECTED_MODE" == "no" ]; then
+	LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.ProtectedMode=no"
+fi
 
 export GO15VENDOREXPERIMENT=1
 
@@ -31,13 +39,6 @@ if [ "$NOCOPY" != "1" ]; then
 fi
 
 core/gen.sh
-
-LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.Version=${VERSION}"
-LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.GitSHA=$(git rev-parse --short HEAD)"
-LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.BuildTime=$(date +%FT%T%z)"
-if [ "$PROTECTED_MODE" == "no" ]; then
-	LDFLAGS="$LDFLAGS -X github.com/tidwall/tile38/core.ProtectedMode=no"
-fi
 
 # build and store objects into original directory.
 go build -ldflags "$LDFLAGS" -o "$OD/tile38-server" cmd/tile38-server/*.go
